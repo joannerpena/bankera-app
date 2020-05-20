6708dab2814c31e4e1b531516fbfb4be909acde2
joannerpena <joanner.c1006@gmail.com>
2020-05-20 04:18:45 -0400

    Added connection to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
