388c397a972f0d1a538465f33b0e920ceeba3fde
Joanner Pena <joanner.c1006@gmail.com>
2020-05-28 19:47:47 -0400

    Added Transaction Model

--- models/Account.js
+++ models/Account.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const AccountSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
+    ref: 'users',
   },
   account_type: {
     type: String,
--- models/Transaction.js
+++ models/Transaction.js
@@ -1,28 +1,31 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const dateNow = new Date.now();
 
-const TransactionSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    },
-    transaction_title: {
-        type: String
-    },
-    transaction_amount: {
-        type: Number
-    },
-    transaction_status: {
-        type: String
-    },
-    transaction_type: {
-        type: String
-    },
-    transaction_date: {
-        type: Date,
-        Default: dateNow
-    }
+const TransactionSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  transaction_title: {
+    type: String,
+    required: true,
+  },
+  transaction_amount: {
+    type: Number,
+    required: true,
+  },
+  transaction_status: {
+    type: String,
+  },
+  transaction_type: {
+    type: String,
+  },
+  transaction_date: {
+    type: Date,
+    Default: dateNow,
+  },
 });
 
 module.exports = Transaction = mongoose.model('transaction', TransactionSchema);
