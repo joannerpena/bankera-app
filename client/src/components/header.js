58edfc7f4d2601e2c288521c084625cf3b6e497e
Joanner Pena <joanner.c1006@gmail.com>
2020-05-30 00:17:19 -0400

    Added Header Component

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 
-class Header extends Component{
-    render(){
-        return(
-            <header>
-                Hello
-            </header>
-        )
-    }
+// @import Assets
+import Logo from '../assets/img/bankera-logo.svg';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <img
+            src={Logo}
+            className="d-inline-block align-top brand-logo"
+            alt="Bankera Logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#">Personal</Nav.Link>
+            <Nav.Link className="ml-3" href="#">
+              Business
+            </Nav.Link>
+            <Nav.Link className="ml-3" href="#">
+              About Us
+            </Nav.Link>
+            <Nav.Link className="ml-3" href="#">
+              ContactUs
+            </Nav.Link>
+            <Button className="ml-3 nav-button" variant="dark">
+              Log In
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
-export default Header
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
+// @import modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container } from 'react-bootstrap';
+
+// @import Style
 import './style/main.scss';
 
 // COMPONENTS
 import Header from './components/header';
 
 const App = () => {
-  return <Header />;
+  return (
+    <Container className="page-margin" fluid>
+      <Header />
+    </Container>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
