33252f520065624dd2599e4fd6fbb4557f10e831
Joanner Pena <joanner.c1006@gmail.com>
2020-05-29 21:56:57 -0400

    Added Sass to the project

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style/main.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// COMPONENTS
+import Header from './components/header';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return <Header />;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
