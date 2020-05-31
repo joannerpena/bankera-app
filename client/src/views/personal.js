1defae03da1f6f7c4a54777c441fc54278c6113d
Joanner Pena <joanner.c1006@gmail.com>
2020-05-31 00:44:07 -0400

    Added CardPersonal component to Personal view

--- client/src/views/personal.js
+++ client/src/views/personal.js
@@ -4,15 +4,19 @@ import { Container } from 'react-bootstrap';
 // Components
 import Header from '../components/header';
 import ImageFluid from '../components/image-fluid';
+import CardPersonal from '../components/card-personal-info';
 import Footer from '../components/footer';
 
 const Home = () => {
   return (
     <div className="personal-page">
-      <Container>
+      <Container className="page-margin" fluid>
         <Header />
       </Container>
       <ImageFluid />
+      <Container fluid>
+        <CardPersonal />
+      </Container>
       <Footer />
     </div>
   );
