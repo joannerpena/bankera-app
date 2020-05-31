494b66d243efc5fd803665751f4c7c61dcb23913
Joanner Pena <joanner.c1006@gmail.com>
2020-05-31 02:53:43 -0400

    Fixed text at ImageFluid component

--- client/src/components/image-fluid.js
+++ client/src/components/image-fluid.js
@@ -4,27 +4,25 @@ import { Jumbotron, Button, Row, Col, Container } from 'react-bootstrap';
 const ImageFluid = () => {
   return (
     <Jumbotron className="image-fluid" fluid>
-      <Container>
-        <Row>
-          <Col lg={6}>
-            <h1>Personal Account with Bankera</h1>
-            <p>
-              Open a Bankera checking account and enjoy benefits such as mobile
-              check deposit, online bill pay, linked accounts and much more.
-            </p>
-            <Row>
-              <Col>
-                <Button variant="light" size="lg">
-                  Learn More
-                </Button>
-                <Button variant="outline-light" size="lg">
-                  Get It Now
-                </Button>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col lg={6}>
+          <h1>Personal Account with Bankera</h1>
+          <p>
+            Open a Bankera checking account and enjoy benefits such as mobile
+            check deposit, online bill pay, linked accounts and much more.
+          </p>
+          <Row>
+            <Col>
+              <Button variant="light" size="lg">
+                Learn More
+              </Button>
+              <Button variant="outline-light" size="lg">
+                Get It Now
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Jumbotron>
   );
 };
